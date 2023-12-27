aa608ecf9ccf418be51e66a59f9a123ecffe4fcd
Djuslun <djuslun@yandex.ru>
2023-12-27 10:56:04 +0300

    add redirects for deploy. base url take out in env file

--- src/store/canvasApiSlice.js
+++ src/store/canvasApiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const canvasApiSlice = createApi({
   reducerPath: 'apiSlice',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/',
+    baseUrl: process.env.REACT_APP_BASE_URL + '/',
     prepareHeaders(headers) {
       headers.set('Content-Type', 'application/json');
       return headers;
